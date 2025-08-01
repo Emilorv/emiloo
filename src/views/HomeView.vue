cc50209872bc1f43f6c9e77561d2af1365cfa33e
EmilOrv <emilols.eo@gmail.com>
2025-08-01 22:51:11 +0200

    feat: initialize sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,18 @@
   <main>
       <section class="top-section">
         <h1 class="title">Emiloo</h1>  
+        <img src="" alt="">
+        <p></p>
+      </section>
+      <section>
+
+      </section>
+      <section class="projects">
+        <h2 class="projects-title title">Prosjekter</h2>
+      </section>
+
+      <section class="side-quests">
+            <h2 class="side-quests-title title">Sidequests</h2>
       </section>
   </main>
 </template>
