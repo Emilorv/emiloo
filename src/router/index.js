d4b88eff05ef4745ced37743db1e81c9a6e66139
EmilOrv <emilols.eo@gmail.com>
2025-08-01 21:21:46 +0200

    feat: initialize component for CalendarOfLife

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TerminalView from "@/views/TerminalView.vue";
+import CalendarOfLife from '@/views/CalendarOfLife.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/cli',
       name:'terminal',
       component: TerminalView
+    },
+    {
+      path: '/livskalender',
+      name: 'livskalender',
+      component: CalendarOfLife
     }
   ]
 })
---
+++ src/views/CalendarOfLife.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <main>
+        <h1 hidden>Livskalenderen</h1>
+        <section>
+            <div>
+                <label>Day:</label>
+                <input type="number" v-model="birthDay" min="1" max="31" />
+
+                <label>Month:</label>
+                <select v-model="birthMonth">
+                    <option disabled value="">Month</option>
+                    <option v-for="(month, index) in months" :key="index" :value="index + 1">
+                    {{ month }}
+                    </option>
+                </select>
+
+                <label>Year:</label>
+                <input type="number" v-model="birthYear" min="1900" :max="currentYear" />
+            </div>
+
+        </section>
+
+    </main>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'CalendarOfLife',
+        data() {
+            return {
+                birthday: null,
+            }
+        }
+    }
+</script>
