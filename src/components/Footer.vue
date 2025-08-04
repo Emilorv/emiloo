8370e8922e57dc75f545fc36d77c71d5e82f223f
EmilOrv <emilols.eo@gmail.com>
2025-08-04 20:48:20 +0200

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <div class="footer__content">
+            <p class="footer__text">Emil Orvik Olsson © 2025</p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    padding: 2rem 0;
+}
+
+.footer__content {
+    text-align: center;
+}
+
+.footer__text {
+    margin: 0;
+    font-size: 1rem;
+    color: #666;
+}
+</style>
